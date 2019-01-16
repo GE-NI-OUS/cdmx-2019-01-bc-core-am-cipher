26a8d45dae3a16be210c61867ab7dd13a9c45bfe
Gen <egenesis.egonzalez@gmail.com>
2019-01-16 11:02:25 -0600

    se crear objeto cipher y función decode

--- src/index.js
+++ src/index.js
@@ -1,55 +1,9 @@
-//Evento botón entrar primera pantalla//
-function comenzar(){
-let boton= document.getElementById('next2');
-boton.getEventListener("click", siguiente, false);
+
+function cifrar (){
+  const cadena = document.getElementById("mensaje").value.toUpperCase();
+  const offset = parseInt(document.getElementById("desplazamiento")).value;
+  document.getElementById("cifra").innerHtml = cipher.enconde(cadena,offset));
 }
-function siguiente (){
-  let div1= document.getElementById('ejemplo');
-  let div2= document.getElementById('pantalla2');
-  div1.style.display="none";
-  div2.style.display="contents";
-}
-//Evento botón logout segunda pantalla//
-function regresar (){
-  let boton_back = document.getElementById('back1');
-  boton.getEventListener("click", atras, false);
-}
-function atras(){
-  let div2 = document.getElementById('pantalla2');
-  let div1 = document.getElementById('ejemplo');
-  div2.style.display="none";
-  div1.style.display="contents";
-}
-//Evento botón continuar segunda pantalla//
-function adelante (){
-  let boton2= document.getElementById('start');
-  boton2.getEventListener("click", continuar, false);
-}
-function continuar(){
- let div2 = document.getElementById('pantalla2');
- let div3 = document.getElementById('pantalla3');
- div2.style.display="none";
- div3.style.display="contents";
-}
-//Evento botón logout tercera pantalla//
-function regresar2(){
-  let boton_back2 = document.getElementById('back2');
-  boton_back2.getEventListener("click", atras2, false);
-}
-function atras2(){
-  let div3 = document.getElementById('pantalla3');
-  let div1= document.getElementById('ejemplo');
-   div3.style.display="none";
-   div1.style.display="contents";
-}
-//Evento botón Instrucciones tercera pantalla//
-function back_listado(){
-  let boton_instrucciones = document.getElementById('instrucciones1');
- boton_instrucciones.getEventListener("click", instrucciones2, false);
-  }
-function instrucciones2(){
-  let div3 = document.getElementById('pantalla3');
-  let div2 = document.getElementById('pantalla2');
-   div3.style.display="none";
-   div2.style.display="contents";
-}
+
+//const boton = document.getElementById('cifra');
+//boton.addEventListener("click",window.cipher.cifrar());
